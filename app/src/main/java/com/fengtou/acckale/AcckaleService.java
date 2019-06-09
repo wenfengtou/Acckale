8ba3c200abec1b79dcce7b02c2a14790ac4af077
wenfeng.li <qianmayi@gmail.com>
2019-06-09 22:21:20 +0800

    初步配置框架

--- app/src/main/java/com/fengtou/acckale/AcckaleService.java
+++ app/src/main/java/com/fengtou/acckale/AcckaleService.java
@@ -4,16 +4,67 @@ import android.accessibilityservice.AccessibilityService;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.accessibility.AccessibilityEvent;
+import android.view.accessibility.AccessibilityNodeInfo;
 
 public class AcckaleService extends AccessibilityService {
+
+    private static final String TAG = "AcckaleService";
 
     public AcckaleService() {
     }
 
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
+        Log.i(TAG, "onAccessibilityEvent " + event.toString());
+        AccessibilityNodeInfo rootNode = getRootInActiveWindow();
+        if (rootNode == null) {
+            Log.i(TAG, "rootNode is　null");
+            return;
+        } else {
+          //  recycle(rootNode);
+        }
+    }
 
+    @Override
+    protected boolean onGesture(int gestureId) {
+        Log.i(TAG, "onGesture " + gestureId);
+        return super.onGesture(gestureId);
+    }
+
+    @Override
+    protected boolean onKeyEvent(KeyEvent event) {
+        Log.i(TAG, "onKeyEvent " + event.toString());
+        return super.onKeyEvent(event);
+    }
+
+    public void recycle(AccessibilityNodeInfo info) {
+        if (info.getChildCount() == 0) {
+            if (info.getText() != null && info.getText().equals("华为工作法")) {
+                Log.i(TAG, "child widget----------------------------" + info.getClassName());
+                Log.i(TAG, "isClickable:" + info.isClickable());
+                Log.i(TAG, "Text：" + info.getText());
+                Log.i(TAG, "windowId:" + info.getWindowId());
+                Log.i(TAG, "windowId:" + info.getWindowId());
+                AccessibilityNodeInfo parent_info = null;
+                if (!info.isClickable())  parent_info = info;
+                if (!parent_info.isClickable()) {
+                    parent_info = parent_info.getParent();
+                }
+                if (parent_info != null) {
+                    Log.i(TAG, "点击了华为工作法！！！！");
+               //     parent_info.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+                }
+            }
+        } else {
+            for (int i = 0; i < info.getChildCount(); i++) {
+                if(info.getChild(i)!=null){
+                    recycle(info.getChild(i));
+                }
+            }
+        }
     }
 
     @Override
